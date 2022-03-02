4ecfe98aec2a6ed0efab730be1efaf0217662a6c
FrankYeah <aaa24295234@gmail.com>
2022-03-03 02:35:04 +0800

    add popup

--- _nuxt/static/1646159087/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fvirtual","\u002Fcontact","\u002Fworks","\u002F","\u002Fabout"]})
--- _nuxt/static/1646159087/works/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FrisingFilmEn\u002F_nuxt\u002Fstatic\u002F1646159087",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks",config:{_app:{basePath:"\u002FrisingFilmEn\u002F",assetsPath:"\u002FrisingFilmEn\u002F_nuxt\u002F",cdnURL:a}}}}(null));
